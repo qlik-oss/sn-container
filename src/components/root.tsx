1d961a91fce366ce4069fc3b85562081731d5ccc
VasilisKalaitzis <cs101092@gmail.com>
2022-09-13 10:16:42 +0200

    feat(initial-commit): initial commit

---
+++ src/components/root.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Container() {
+  const containerStyle = {
+    height: '100%',
+    width: '100%',
+  };
+
+  return (
+    <div className="container-root" style={containerStyle}>
+      Container Root
+    </div>
+  );
+}
