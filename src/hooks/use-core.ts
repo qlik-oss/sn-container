d0e96261f58c2dbcc5ac1d6de5ac478537b8f1cb
VasilisKalaitzis <cs101092@gmail.com>
2022-09-21 11:26:52 +0200

    feat(theme): fix github comments

--- src/hooks/use-core.ts
+++ src/hooks/use-core.ts
@@ -1,9 +1,8 @@
-import { useElement, useLayout, useState, useEffect  } from '@nebula.js/stardust';
+import { useElement, useState, useEffect } from '@nebula.js/stardust';
 import useViewState from './use-view-state';
 
 const useCore = () => {
   const element = useElement();
-  const layout = useLayout();
   const viewState = useViewState();
 
   const [core, setCore] = useState();
@@ -11,9 +10,8 @@ const useCore = () => {
     if (!viewState || !element) return undefined;
     setCore({
       element,
-      layout,
     });
-  }, [viewState, element, layout]);
+  }, [viewState, element]);
 
   return core;
 };
