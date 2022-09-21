afa37c6d059a0b61d7da2e7ebfccb9de16900f58
VasilisKalaitzis <cs101092@gmail.com>
2022-09-21 13:41:39 +0200

    feat(theme): fix github comments

--- src/services/layout-service/meta.js
+++ src/services/layout-service/meta.js
@@ -1,5 +1,3 @@
-import { getValue } from 'qlik-chart-modules';
-
 export default function createLayoutServiceMetaFn() {
   return function layoutServiceMeta({ layout }) {
     const isSnapshot = !!layout.snapshotData;
--- src/services/theme-service/__tests__/theme-style-matric.spec.js
+++ src/services/theme-service/__tests__/theme-style-matric.spec.js
@@ -1,4 +1,1 @@
-// eslint-disable-next-line no-unused-vars
-import themeStyleMatrix from '../theme-style-matrix';
-
 describe('theme-style-matrix', () => {});
