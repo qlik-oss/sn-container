6d6299d320f738509995c07272d5fb029c527915
VasilisKalaitzis <cs101092@gmail.com>
2022-09-28 13:25:03 +0200

    feat(types): add appropriate types

--- src/mocks/models.ts
+++ src/mocks/models.ts
@@ -1,11 +1,9 @@
 import layoutService from './layout-service';
-import { themeService as createThemeService } from 'qlik-chart-modules';
-import themeStyleMatrix from '../services/theme-service/theme-style-matrix';
 
 export default {
   containerModel: {
     layoutService,
-    themeService: createThemeService({ theme: 'light', styleMatrix: themeStyleMatrix }),
+    themeService: { getTheme: () => 'light' },
     embed: { render: jest.fn(async () => Promise.resolve({ data: {} })) },
     app: {},
     options: { direction: 'ltr' },
